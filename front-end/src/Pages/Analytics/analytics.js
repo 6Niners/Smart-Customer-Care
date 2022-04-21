2d043e337be7f6b6d2a02d04bccbc4b15490e518
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2022-04-21 20:41:40 +0200

    Squashed commit of the following:
    commit 325db1e8aacb0b41e5982dd7bd439b0ab4e662e2
    Author: Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
    Date:   Fri Apr 15 13:29:44 2022 +0200
    
        Sentiment score calculator ignores neutral
    
    commit b952f2231b0cb03120fe2482f3b92335ff5fc39b
    Author: Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
    Date:   Wed Apr 6 15:43:40 2022 +0200
    
        Refactoring - (read description)
    
        All UI element data are now managed by states :ambulance:
        - Sentiment,
        - Daily comparison values
        - Aggregations,
        - Top10 words graph
    
        Remaining:
        - weekly tweet length graph
    
        Commit also includes changes to progress circle file names and imports
    
    commit 76e3b63c8571ca95dcbd69a0e8fbf6dfe7c4b10f
    Author: Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
    Date:   Mon Apr 4 21:04:50 2022 +0200
    
        simplified emotion page
    
    commit 751913809d9d2ad16562945e43f054e54c31e0b2
    Author: Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
    Date:   Thu Mar 31 12:41:01 2022 +0200
    
        Updated statistics values using firebase - (NEEDS MAJOR REFACTORING AND EXCEPTION CONTROL)
    
    commit 58f63c618c27d48adf4ed558c0e7cce1ba6977b9
    Merge: f76f773 3f7fd9f
    Author: Mohammad Atef Refaie <m.refaie@nu.edu.eg>
    Date:   Wed Mar 30 12:04:43 2022 +0200
    
        Merge branch 'Front-End' of https://github.com/6Niners/Smart-Customer-Care into Front-End
    
    commit f76f7739261c4dbf2dcef80dd910047ce333da11
    Author: Mohammad Atef Refaie <m.refaie@nu.edu.eg>
    Date:   Wed Mar 30 12:01:48 2022 +0200
    
        Added Progress Circular Bar
    
    commit 3f7fd9f910241effe9466a5658f86101ee8ff6d5
    Author: Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
    Date:   Wed Mar 30 11:39:45 2022 +0200
    
        Added Firebase configuration data
    
    commit 66ca2aa379edb2d78d7b10f677bd0a9c7e814386
    Author: Mohammad Atef Refaie <m.refaie@nu.edu.eg>
    Date:   Tue Mar 29 12:29:04 2022 +0200
    
        Top 10 objects returns objects and has a graph
    
    commit 95ab01a7d3e944c3a916eb00badf05f5b861de88
    Author: Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
    Date:   Mon Mar 28 22:06:36 2022 +0200
    
        Added dynamic daily value comparisons for statistics cards
    
        +ve difference -> (plus) value and green upper arrow
    
        -ve difference -> (minus) value and down red arrow
    
    commit 0d1e9c418a4da4a4e02c40eca3bdc70b42a6911d
    Author: Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
    Date:   Sun Mar 27 16:13:10 2022 +0200
    
        added extra card for post count
    
    commit f228850ea9e61d38baa25fc8179b8793fefd2283
    Author: Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
    Date:   Sat Mar 26 22:31:44 2022 +0200
    
        Refactoring of code structure and components to make it more modular and organized - (DID NOT REFACTOR EMOTION PAGE)
    
    commit e3c4a5bd88e15a4f5ab3707ed1a98c688382124d
    Author: Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
    Date:   Sat Mar 26 20:15:31 2022 +0200
    
        Front-end migration to another branch / added stat code
    
        - mean median mode algos and displayed on analytics page

---
+++ front-end/src/Pages/Analytics/analytics.js
@@ -0,0 +1,91 @@
+import "./analytics.css"
+import Aggregation_Card from "../../Components/Featured Components/Aggregation Card/aggregation_card"
+import Bar_Chart from "../../Components/Featured Components/Graph Components/Bar Chart/bar_chart"
+import LineGraph from "../../Components/Featured Components/Graph Components/Line Graph/line_graph"
+
+
+
+
+const Analytics = ({ aggCardState, weeklyAvgList, top10State, dailyComparisonState }) => {
+
+  
+  return (
+    <div id="analytics-showcase">
+      <div id="aggregation-showcase">
+        <div className="row">
+          <div className="col-lg-3 col-md-6">
+            <div className="featured-item">    
+              <Aggregation_Card 
+                  Title={aggCardState[3]["title"]} 
+                  Aggregation={aggCardState[3]["aggregation"]} 
+                  Rating={aggCardState[3]["rating"]}
+                  comparisonValue={dailyComparisonState["count"]} 
+                  Update_Rate="Daily" 
+              />
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6">
+            <div className="featured-item">
+              <Aggregation_Card 
+                  Title={aggCardState[0]["title"]} 
+                  Aggregation={aggCardState[0]["aggregation"]}
+                  Rating={aggCardState[0]["rating"]}
+                  comparisonValue={dailyComparisonState["average"]}
+                  Update_Rate="Daily" 
+              />
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6">
+            <div className="featured-item">
+              <Aggregation_Card 
+                  Title={aggCardState[1]["title"]} 
+                  Aggregation={aggCardState[1]["aggregation"]}
+                  Rating={aggCardState[1]["rating"]}
+                  comparisonValue={dailyComparisonState["median"]}
+                  Update_Rate="Daily" 
+              />  
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6">
+            <div className="featured-item">    
+              <Aggregation_Card 
+                  Title={aggCardState[2]["title"]} 
+                  Aggregation={aggCardState[2]["aggregation"]} 
+                  Rating={aggCardState[2]["rating"]}
+                  comparisonValue={dailyComparisonState["mode"]} 
+                  Update_Rate="Daily" 
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="avg-length-bar-showcase" className="featured-item">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="title-primary">Weekly Average Length</h3>
+            <LineGraph data={weeklyAvgList} xAxis='name' yAxis='avgLength'/>
+          </div>
+        </div>
+      </div>
+      
+      <div id="avg-length-bar-showcase" className="featured-item">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="title-primary">Top10 Mentioned Words</h3>
+            <Bar_Chart data={top10State} xAxis='word' yAxis='occurences'/>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+
+
+export default Analytics
