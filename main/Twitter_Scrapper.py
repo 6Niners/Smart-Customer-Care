483e0da0eba1655d6fe1a83ba50d753330418f7c
Ahmed-Abbas-Affan <ahmed.abbas12498@yahoo.com>
2022-04-21 15:55:48 +0200

    remove day column

--- main/Twitter_Scrapper.py
+++ main/Twitter_Scrapper.py
@@ -90,6 +90,7 @@ def Get_Scrapped_Data(searchKeyword):
     df["Tweet"]= remove_punc(df['Tweet'])
     df["day"]= extract_date(df["Time"])
     df = df.drop(columns=['Time'])
+    df = df.drop(columns=['day'])
     df = get_df_for_day_before(df)
     return df
 
