00b61d50729d34cb57d94303611e790f6398918e
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2022-04-24 15:23:30 +0200

    Added local server communication support between front-end and back-end (READ DESC)
    FASTAPI:
     - Installed axion, uvicorn and fastapi dependencies
       for communication under the http protocol
    
    -  Created a post method to send keyword to server
    
    Backend
     - updated main with FastAPI dependencies and
       methods
    
    - main page calls the backend with the keyword
       sent through the fastapi server
    
    - updated firebase upload code to return a response
       (only works for success case)
    
    - added the firebaseAuth function as a parameter to the upload function to avoid an exception caused by repeated authentication calls
    
    Frontend:
     - added new scrapper page which includes an input
       field and button to start scrapping
    
     - Frontend uses post method to send keyword to
       the server and update relevant states
    
    - Current Issues:
      - Add exceptions to all requests especially the
        firebase upload function when called from the
        server
    
     - front end statistics need to be updated to get new
        data when its released

--- back-end/main/main.py
+++ back-end/main/main.py
@@ -1,7 +1,44 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import string
+
 from Twitter_Scrapper import Get_Scrapped_Data
 from Text_Normalization import Text_Normalization
-from Database_Firebase import uploadDataToFirebase
+from Database_Firebase import uploadDataToFirebase, getFirebaseAuth
 
-keyword = "فودافون"
 
-uploadDataToFirebase("Twitter Data Before Modelling", keyword, Text_Normalization(Get_Scrapped_Data(keyword), 'Tweet'), 2500)
+class ScrapperKeyword(BaseModel):
+  keyword : str
+
+
+
+originsList = ["http://localhost:3000"]
+
+fbAuth = getFirebaseAuth()
+
+apiApp = FastAPI()
+apiApp.add_middleware(
+  CORSMiddleware,
+  allow_origins = originsList,
+  allow_credentials = True,
+  allow_methods = ["*"],
+  allow_headers = ["*"]
+)
+
+
+
+@apiApp.get("/")
+def default():
+  return {"Connection" : "Is Working"}
+
+@apiApp.post("/api/keyword")
+def post_keyword(_keyword : ScrapperKeyword):
+  response = uploadDataToFirebase("Test", _keyword.keyword, Text_Normalization(Get_Scrapped_Data(_keyword.keyword), 'Tweet'), 2500, fbAuth)
+  if response:
+    return "Operation Successful"
+  return HTTPException(404, "Operation Failed")
+
+
+# keyword = "فودافون"
+# uploadDataToFirebase("Test", keyword, Text_Normalization(Get_Scrapped_Data(keyword), 'Tweet'), 2500)
