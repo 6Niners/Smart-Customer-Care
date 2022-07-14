9db74bc84b0f5e0e4e191cd3b6c23dbb86525212
Mohamed-H-Anwar <mohamedpc.anwar@gmail.com>
2022-07-15 00:42:30 +0200

    Removed Sensitive Data

--- front-end/src/Api/firebase.js
+++ front-end/src/Api/firebase.js
@@ -1,15 +1,26 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import dotenv from 'dotenv'
 
+dotenv.config();
+const {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+} = process.env;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6HQPfo8V0V_BRwJx6MNMeq58u_MOHkSQ",
-  authDomain: "smartcc-960f7.firebaseapp.com",
-  projectId: "smartcc-960f7",
-  storageBucket: "smartcc-960f7.appspot.com",
-  messagingSenderId: "857055861649",
-  appId: "1:857055861649:web:9e7060fd259d56e1815830",
-  measurementId: "G-N25P91NF69"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId
 };
 
 
